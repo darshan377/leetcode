f217d9b1fe693ba7b6513d80ad6dd20d5b0f51a3
darshan377 <darshan377>
2024-09-17 19:55:58 +0600

    ting ting

---
+++ greedy/11. Container With Most Water.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int l = 0;
+        int r = height.length - 1;
+        int max = 0;
+
+        while (l<r){
+            int w = r - l;
+            int h = Math.min(height[l],height[r]);
+            int area = w * h;
+            max = Math.max(max,area);
+
+            if(height[l]<height[r]){
+                l++;
+            }
+            else if(height[l]>height[r]){
+                r--;
+            }
+            else{
+                l++;
+                r--;
+            }
+        }
+
+        return max;
+    }
+}
